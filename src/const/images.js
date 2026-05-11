27e49c3685aae7c30defef1ea4b5dff42fdfa822
David Triminio <142177715+davidtriminio@users.noreply.github.com.>
2026-05-11 15:59:24 -0600

    chore: add correct Banner image name

--- src/const/images.js
+++ src/const/images.js
@@ -1,10 +1,12 @@
 import logo from '../assets/images/logo.svg';
+import Banner from '../assets/images/Banner.png' ;
 import project1 from '../assets/images/project1.png'
 import project2 from '../assets/images/project2.png'
 import project3 from '../assets/images/project3.png'
 
 export const IMAGES = {
     logo,
+    Banner,
     project1,
     project2,
     project3,
