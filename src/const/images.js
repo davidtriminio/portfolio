4349872f84342f8e53890f6e61caaa39fefb909f
David Triminio <142177715+davidtriminio@users.noreply.github.com.>
2026-05-11 15:56:39 -0600

    chore: remove unused banner image

--- src/const/images.js
+++ src/const/images.js
@@ -1,12 +1,10 @@
 import logo from '../assets/images/logo.svg';
-import banner from '../assets/images/banner.png' ;
 import project1 from '../assets/images/project1.png'
 import project2 from '../assets/images/project2.png'
 import project3 from '../assets/images/project3.png'
 
 export const IMAGES = {
     logo,
-    banner,
     project1,
     project2,
     project3,
