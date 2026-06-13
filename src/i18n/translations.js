c3334fc33e1898cdc3c0ed772a24c710e0df7fc0
David Triminio <142177715+davidtriminio@users.noreply.github.com>
2026-06-12 19:39:35 -0600

    feat: update CV

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -152,7 +152,7 @@ export const translations = {
       },
       cvLabel: "View resume",
       cvAriaLabel: "Open David Triminio resume in a new tab",
-      cvHref: "/David-Triminio-CV-EN.pdf",
+      cvHref: "/David-Triminio-CV_EN.pdf",
     },
     about: {
       greeting: "Hi, I'm",
